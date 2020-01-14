e0bd731785a76e0f0d08994e7b7ef6b7f0e3a6fe
gaewook <gaewook@DESKTOP-3ENQJR5>
2020-01-14 10:38:19 +0900

    MyBatis 연동

--- board/src/test/java/examples/springmvc/dao/UserMapperTest.java
+++ board/src/test/java/examples/springmvc/dao/UserMapperTest.java
@@ -1,6 +1,8 @@
 package examples.springmvc.dao;
 
 import examples.springmvc.config.ApplicationConfig;
+import examples.springmvc.dto.User;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,5 +17,13 @@ import java.util.List;
 @ContextConfiguration(classes = {ApplicationConfig.class})
 @Transactional // test코드가 실행된 후, 롤백되기 때문에 원본 데이터는 문제가 발생하지 않는다. 해당 어노테이션을 붙이지 않으면 25번째 줄에서 오류가 발생할 수 있다. 테스트 메소드의 실행순서는 메소드 선언 순이 아니기 때문이다.
 public class UserMapperTest {
+	@Autowired
+	UsersMapper userMapper;
+	
+	@Test
+	public void getUsers() throws Exception{
+		List<User> users = userMapper.getUsers();
+		Assert.assertEquals(users.size(), 2);
+	}
 }
 
