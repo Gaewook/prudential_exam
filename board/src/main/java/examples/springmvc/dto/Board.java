df2074351b582a5494c62b2ef5f901d3124b7732
gaewook <gaewook@gmail.com>
2020-01-16 10:39:03 +0900

    로그인 페이지 구

--- board/src/main/java/examples/springmvc/dto/Board.java
+++ board/src/main/java/examples/springmvc/dto/Board.java
@@ -3,69 +3,84 @@ package examples.springmvc.dto;
 import java.sql.Date;
 
 public class Board {
-    Long id;
-    String userId;
-    String userName;
-    public String getUserName() {
+	Long id;
+	String userId;
+	String userName;
+	String title;
+	String content;
+	Integer readCount;
+	Date writeDate;
+	Date updateDate;
+
+	public String getUserName() {
 		return userName;
 	}
+
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
 
-	String title;
-    String content;
-    Integer readCount;
-    Date writeDate;
-	Date updateDate;
-    
-    public Long getId() {
+	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getUserId() {
 		return userId;
 	}
+
 	public void setUserId(String userId) {
 		this.userId = userId;
 	}
+
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
 	public String getContent() {
 		return content;
 	}
+
 	public void setContent(String content) {
 		this.content = content;
 	}
+
 	public Integer getReadCount() {
 		return readCount;
 	}
+
 	public void setReadCount(Integer readCount) {
 		this.readCount = readCount;
 	}
+
 	public Date getWriteDate() {
 		return writeDate;
 	}
+
 	public void setWriteDate(Date writeDate) {
 		this.writeDate = writeDate;
 	}
+
 	public Date getUpdateDate() {
 		return updateDate;
 	}
+
 	public void setUpdateDate(Date updateDate) {
 		this.updateDate = updateDate;
 	}
+
 	@Override
 	public String toString() {
 		return "Board [id=" + id + ", userId=" + userId + ", userName=" + userName + ", title=" + title + ", content="
 				+ content + ", readCount=" + readCount + ", writeDate=" + writeDate + ", updateDate=" + updateDate
 				+ "]";
 	}
-	
+
 }
